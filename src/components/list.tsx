4bcd6ac05bbb59ef1e89151d889fdc3cf7769cd9
Ralph Kube <ralph.kube@uit.not>
2024-01-14 20:25:53 -0800

    Styled plot list in Chakra

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,7 +1,12 @@
 
-import { Button } from '@chakra-ui/react'
+import { Button, SimpleGrid, List, ListItem, Stack, Text } from '@chakra-ui/react'
+import{ DeleteIcon } from '@chakra-ui/icons'
 import { items_t } from '../types/all_types'
 // import { ReactNode } from 'react'
+
+// Try using fontawesome here:
+<script src="https://kit.fontawesome.com/7429b4e975.js" crossorigin="anonymous"></script>
+
 
 // The interface describes what to expect as a parameter
 interface signal_list_i {
@@ -13,17 +18,21 @@ interface signal_list_i {
 // Renders items_list
 const MyList = ({ signal_list, render, cb}: signal_list_i) => {   
 
-    // const new_cb = (ix: any) => {console.log('new_c, ix = ', ix)}
-
     return (
-        <ul>
-        { signal_list.map((item, ix) => ( 
-        <li key={ix}> 
-            {render(item)}
-             <Button colorScheme='teal' size='xs' variant='ghost' onClick={() => cb(ix)}>-</Button> 
-             {/* <Button onClick={() => {console.log("clicked")}}>button</Button> */}
-        </li>)) }
-        </ul>
+        <SimpleGrid gap={12} px={12} columns={2}>
+
+
+        <List size="l" variant="custom" spacing={2}>
+            {signal_list.map((item, ix) => (
+                <ListItem key={ix}>
+                    <Stack direction='row'>
+                    <Text color="black"> {render(item)} </Text>
+                    <Button onClick={() => cb(ix)} size='l'> <DeleteIcon/> </Button>
+                    </Stack>
+                </ListItem>
+            ))}
+        </List>
+        </SimpleGrid>
     )
 }
 
